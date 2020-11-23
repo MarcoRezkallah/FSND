e0f5470ae1630d1c88cb32fc99f5fdb3dd7ccb91
Marco Rezkallah <mmsrezkallah@gmail.com>
2020-11-23 21:49:06 +0200

    Implement GET one Artist

--- projects/01_fyyur/starter_code/helper.py
+++ projects/01_fyyur/starter_code/helper.py
@@ -34,7 +34,6 @@ class helper:
 
     def map_venue(venue):
         genres = json.loads(venue.genres)
-        print(json.loads(venue.genres))
         out = {
             "id": venue.id,
             "name": venue.name,
@@ -47,6 +46,36 @@ class helper:
             "facebook_link": venue.facebook_link,
             "seeking_talent": venue.seeking_talent or False,
             "image_link": venue.image_link,
+            "past_shows": [{
+                "artist_id": 5,
+                "artist_name": "Matt Quevedo",
+                "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+                "start_time": "2019-06-15T23:00:00.000Z"
+            }],
+            "upcoming_shows": [{
+                "artist_id": 6,
+                "artist_name": "The Wild Sax Band",
+                "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+                "start_time": "2035-04-01T20:00:00.000Z"
+            }],
+            "past_shows_count": 1,
+            "upcoming_shows_count": 1,
+        }
+        return out
+
+    def map_artist(artist):
+        genres = json.loads(artist.genres)
+        out = {
+            "id": artist.id,
+            "name": artist.name,
+            "genres": genres,
+            "city": artist.city,
+            "state": artist.state,
+            "phone": artist.phone,
+            "website": artist.website,
+            "facebook_link": artist.facebook_link,
+            "seeking_talent": artist.seeking_venue or False,
+            "image_link": artist.image_link,
             "past_shows": [{
                 "artist_id": 5,
                 "artist_name": "Matt Quevedo",
